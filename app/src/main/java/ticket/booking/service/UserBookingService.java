77ff5cfc4c6e0cc4de68ce1c919b7902c68532f1
roshanvrazak <roshanvrazak@gmail.com>
2025-02-26 22:16:15 +0000

    local db added data reading from file

--- app/src/main/java/ticket/booking/service/UserBookingService.java
+++ app/src/main/java/ticket/booking/service/UserBookingService.java
@@ -1,5 +1,29 @@
 package ticket.booking.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.omg.CORBA.Object;
+import ticket.booking.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class UserBookingService {
 
+    private User user;
+
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    // Constructor initialising User logged in
+    public UserBookingService(User user1) throws IOException {
+        this.user = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+        });
+    }
 }
