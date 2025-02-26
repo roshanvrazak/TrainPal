5cb392867aab3f306566d2a6a48739110c414847
roshanvrazak <roshanvrazak@gmail.com>
2025-02-26 20:24:01 +0000

    Initial commit

---
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -0,0 +1,13 @@
+package ticket.booking.entities;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class Train {
+    private String trainId;
+    private int trainNo;
+    private List<List<Integer>> seats;
+    private Map<String, Date> stations;
+
+}
---
+++ app/src/main/java/ticket/booking/service/UserBookingService.java
@@ -0,0 +1,5 @@
+package ticket.booking.service;
+
+public class UserBookingService {
+
+}
